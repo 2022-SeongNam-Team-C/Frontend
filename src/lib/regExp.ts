665cfaa1178da0261393cbf5221370b56868b384
UniTaeMin <onnu0802@naver.com>
2022-09-19 00:13:56 +0900

    Feat: RegExp

---
+++ src/lib/regExp.ts
@@ -0,0 +1,30 @@
+export type returnType = {
+  status: boolean;
+  message: string;
+};
+
+class RegExp {
+  private email =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+  private password = /^.*(?=^.{6,24}$)(?=.*\d)(?=.*[a-zA-Z]).*$/; //비밀번호는 영문과 숫자를 포함하여 6~24자리
+  private username = /^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,20}$/; //2~20자리
+
+  public SignInRegEx(email: string, password: string): Promise<returnType> {
+    return new Promise((resolve, reject) => {
+      console.log(email, password);
+      if (!this.email.test(email)) {
+        resolve({ status: false, message: "올바른 이메일을 입력해주세요." });
+      } else if (!this.password.test(password)) {
+        resolve({
+          status: false,
+          message:
+            "비밀번호는 영문과 숫자를 포함하여 6~24자리 내로 작성해주세요.",
+        });
+      } else {
+        resolve({ status: true, message: "올바른 정규식 입니다." });
+      }
+    });
+  }
+}
+
+export default new RegExp();
